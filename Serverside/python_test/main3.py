7e17fa6f42920451b678056f7e20e7d45099e699
DESKTOP-JB24B0S\Scott Burgert <quantumech3@gmail.com>
2019-02-02 15:10:06 -0500

    Made asyncio demo exit when 'exit' command is entered

--- Serverside/python_test/main3.py
+++ Serverside/python_test/main3.py
@@ -2,10 +2,9 @@ import asyncio
 
 # Meant to simulate the Google Sheets API and the
 async def asyncFunc():
-	while(True):
+	while True:
 		print("Xd")
 		# Tells thread to complete after a second
-		await asyncio.sleep(1)
 		print("Xd") # Therefore this statement will never execute
 
 async def console():
@@ -13,15 +12,14 @@ async def console():
 		cmd = input()
 
 		if cmd == "exit":
-			exit(0)
+			asyncio.get_event_loop().stop()
+			return
 
 async def mainAsync():
 	# First thread being scheduled
 	asyncio.ensure_future(asyncFunc())
 	# Second thread being scheduled
 	asyncio.ensure_future(console())
-	# TODO: Figure out how to make a 'Task exception' handler
-	# TODO: Once you figure that out, then write down how this will apply to WebSocket vs the console
 
 # Get event stack
 loop = asyncio.get_event_loop()
