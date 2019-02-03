7ba1d4c1d7b95600b6eb317b137e8486d0fb7eb0
NoNameYet <quantumech3@gmail.com>
2019-02-03 02:57:27 -0500

    jsonTest works now

--- Serverside/python_test/jsonTest.py
+++ Serverside/python_test/jsonTest.py
@@ -2,8 +2,22 @@ import json
 
 
 def open_json(path):
-    with open(path, 'w') as file:
-        return json.load(file)
+
+    # If a file exists
+    try:
+        # Open the file and load the data as a dictionary
+        with open(path, 'r+') as file:
+            return json.load(file)  # Loads data using file object
+
+    # If the file does not exist, then create it
+    except FileNotFoundError:
+        jsonTxt = "{}"
+
+        # Create new json with minimum required data to be concidered a json
+        with open(path, 'w+') as file:
+            file.write(jsonTxt)
+            return json.loads(jsonTxt)  # json.loads turns a string into a json, hence the extra 's' vs json.load
 
 
-print(open_json("json.json"))
+json.dump({"Hello": "He"}, open("lol", "w+"))
+print(open_json("json.json")["sdsd"])
