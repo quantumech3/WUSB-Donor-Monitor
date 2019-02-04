84a20a6e374f46a646ee8e2afd92667b6b26668d
DESKTOP-JB24B0S\Scott Burgert <quantumech3@gmail.com>
2019-02-04 16:41:00 -0500

    Http hosts websockets server in test!

--- Serverside/python_test/websocketTestMultithreaded.py
+++ Serverside/python_test/websocketTestMultithreaded.py
@@ -1,16 +1,19 @@
 import asyncio
 import websockets
 import threading
+import http.server
+import socketserver
 
-
-
-def webThread():
+def webSocket():
     # Gets called when someone logins to websocket server
     async def handleLogin(websocket, path):
         print("A new friend logged in :D")
 
         # Send the text 'a message' to client
         await websocket.send("A message")
+
+        # Print message sent back from client
+        print(await websocket.recv())
 
     # Create an event loop just for this thread.
     # You must do this BEFORE you instantiate a socket server otherwise...
@@ -19,17 +22,26 @@ def webThread():
     asyncio.set_event_loop(loop)
 
     # Instantiate Websocket server
-    start_server = websockets.serve(handleLogin, "localhost", 8080)
+    start_server = websockets.serve(handleLogin, "localhost", 8000)
 
     # Launch server as async process
     loop.run_until_complete(start_server)
 
-    # Run forever after login is handled
+    # Run loop continuously to allow checking for login
     loop.run_forever()
 
 
-socket = threading.Thread(target=webThread, name="t1")
-socket.start()
+def httpServer():
+    reqHandler = http.server.SimpleHTTPRequestHandler
+
+    with socketserver.TCPServer(("", 8080), reqHandler) as server:
+        server.serve_forever()
+
+
+socketThread = threading.Thread(target=webSocket)
+httpThread = threading.Thread(target=httpServer)
+socketThread.start()
+httpThread.start()
 
 # Something that happens on main thread
 print("pi")
