1850217469e7aec4532b06fea1dcfc35816b5ad3
DESKTOP-JB24B0S\Scott Burgert <quantumech3@gmail.com>
2019-02-07 17:58:44 -0500

    Flask can host website!

---
+++ Serverside/python_test/flaskTest.py
@@ -0,0 +1,22 @@
+from flask import Flask, send_file, send_from_directory
+
+app = Flask(__name__)
+
+# When client requests root, send client index.html
+@app.route("/")
+def home():
+    # Sends data inside index.html back to client
+    return send_file("webpage/index.html")
+
+# 'path', inside of @app.route(), is a variable whose type is ironically named 'path' (like string but allows slashes)
+# Variables specified inside of app.route get mapped to parameters in function
+# Variables in route() are defined by <type: name>
+@app.route("/<path:path>")
+def res(path):
+    # Sends string back to client from whatever path the client requested inside ./webpage/*
+    # YOU SHOULD NOT PUT A SLASH BEFORE THE FIRST ARGUEMENT IN send_file BECAUSE IT WILL NOT LOAD THE FILE BECAUSE
+    # IT WOULD TRY TO LOAD FROM './/webpage/*' which doesnt make any sence
+    return send_file("webpage/"+path)
+
+if __name__ == "__main__":
+    app.run(port=8000)
