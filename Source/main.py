db9bfb68dba4b7f04f214865bea628cd69b13a61
DESKTOP-JB24B0S\Scott Burgert <quantumech3@gmail.com>
2019-02-19 22:04:37 -0500

    Finished client-side.

--- Source/main.py
+++ Source/main.py
@@ -10,3 +10,39 @@ Module description:
 '''
 
 
+from flask import Flask, send_from_directory
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socket = SocketIO(app)
+
+
+@app.route("/")
+def homepage():
+    return send_from_directory('Website', 'index.html')
+
+@app.route("/<path:path>")
+def other(path):
+    return send_from_directory("Website", path)
+
+@socket.on("connect")
+def new_connection():
+    socket.emit('pageData', {
+        'name': 'WUSB R 2019',
+        'startDate': '2/19/19',
+        'endDate': '2/20/19',
+        'goalHourly': 2000,
+        'goalDaily': 4000,
+        'goalWeekly': 8000,
+        'goalTotal': 16000,
+        'pledges': [{
+            'firstName': 'Bobby',
+            'city': 'Port Jefferson',
+            'amtDonated': 4297,
+            'pledgeType': 0,
+            'paidByCredit': True,
+            'isPaid': True
+        }]
+    })
+
+socket.run(app, host='0.0.0.0', port=8080)
